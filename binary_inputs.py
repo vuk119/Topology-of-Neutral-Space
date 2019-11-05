a2d24251f5160bd44ceaf1d909f871a860753ea4
unknown <vuk119@gmail.com>
2019-11-05 17:00:45 +0000

    Input class created

---
+++ binary_inputs.py
@@ -0,0 +1,47 @@
+import numpy as np
+import itertools
+
+
+
+class input:
+
+    def __init__(self, size):
+        '''
+        Object that holds all possible binary inputs of size n and maps them.
+
+        size - size of the input
+        '''
+
+        self.n = size
+        self.inputs = self.generate_inputs()
+        self.class_to_output, self.output_to_class = self.generate_output_class_maps()
+
+    def generate_inputs(self):
+        '''
+        Generates all the possible inputs of size n.
+        '''
+
+        return list(itertools.product([0, 1], repeat=self.n))
+
+    def generate_output_class_maps(self):
+        output_class_map = {}
+        class_output_map = {}
+
+        outputs = list(itertools.product([0, 1], repeat=2**self.n))
+        classes = list(range(0,2**(2**self.n)))
+
+        output_to_class = dict(zip(outputs,classes))
+        class_to_output = dict(zip(classes, outputs))
+
+        return class_to_output, output_to_class
+
+    def get_output_from_class(self, clas):
+        return self.class_to_output[clas]
+
+    def get_class_from_output(self, output):
+        return self.output_to_class[output]
+
+
+input = input(4)
+print(input.generate_inputs())
+print(input.class_to_output)
